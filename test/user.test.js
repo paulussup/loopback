21b30593acd4ef2f92a24c968cc7c24bb63b8c8f
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2016-08-02 10:59:21 +0200

    Update dependencies to their latest versions

--- test/user.test.js
+++ test/user.test.js
@@ -1674,7 +1674,7 @@ describe('User', function() {
           .end(function(err, res) {
             if (err) return done(err);
 
-            assert.deepEqual(res.body, { });
+            assert.deepEqual(res.body, '');
 
             done();
           });
